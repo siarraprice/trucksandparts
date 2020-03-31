dc58d2909cd092dfff76f782eb432aaac1353f6a
Siarra Price <siarraprice@ufl.edu>
2020-03-31 16:01:51 -0500

    Create service and about layout framework

---
+++ page-about.php
@@ -0,0 +1,39 @@
+<?php
+  /*
+  Template Name: About Page Layout
+  Template Post Type: page
+  */
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+  <div class="row">
+
+  </div> <!-- row -->
+
+
+  <div class="row">
+
+  </div> <!-- row -->
+
+
+  <div class="row">
+    <div class="col-md-4">
+
+    </div>
+
+    <div class="col-md-4">
+
+    </div>
+
+    <div class="col-md-4">
+
+    </div>
+  </div> <!-- row -->
+
+</div> <!-- container -->
+
+
+<?php get_footer(); ?>
---
+++ page-service.php
@@ -0,0 +1,29 @@
+<?php
+  /*
+  Template Name: Service Page Layout
+  Template Post Type: page
+  */
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+  <div class="row">
+
+  </div> <!-- row -->
+
+
+  <div class="row">
+
+  </div> <!-- row -->
+
+
+  <div class="row">
+
+  </div> <!-- row -->
+
+</div> <!-- container -->
+
+
+<?php get_footer(); ?>
