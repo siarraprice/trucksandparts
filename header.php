cffbc464072ce5fe538ee465f87f01230e58787a
Kelly DeMent <kdement@ufl.edu>
2020-04-01 12:42:31 -0400

    Update header

--- header.php
+++ header.php
@@ -15,18 +15,24 @@
 
   <body>
     <header>
-      <h1><a href="<?php echo get_home_url();?>"><?php bloginfo('name'); ?></a></h1>
+        <div class="col-md-3">
+            <?php if(get_header_image() == ''){?>
+                <h1><a href="<?php get_home_url();?>"><?php bloginfo('name'); ?></a></h1><?php
+            }else{?>
+                <a href="<?php get_home_url();?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height;?>" width="<?php echo get_custom_header()->width;?>" alt="logo" /></a>
+            <?php } ?>
+        </div>
 
 
-      <div class="col-lg-12 d-flex align-items-center nav-bar-full">
-          <nav class="top-nav">
-          <?php
-            if(has_nav_menu('top-menu')){
-              wp_nav_menu(array('theme_location' => 'top-menu' , 'container_class' => 'top-menu-class'));
-            }else{
-              echo "Please select a top menu through the dashboard";
-            }
-            ?>
-          </nav>
-      </div>
+        <div class="col-lg-12 d-flex align-items-center nav-bar-full">
+            <nav class="top-nav">
+            <?php
+                if(has_nav_menu('top-menu')){
+                    wp_nav_menu(array('theme_location' => 'top-menu' , 'container_class' => 'top-menu-class'));
+                }else{
+                    echo "Please select a top menu through the dashboard";
+                }
+                ?>
+            </nav>
+        </div>
     </header>
